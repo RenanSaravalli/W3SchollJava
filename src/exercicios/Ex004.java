2f800ea3490a8d97b7e55843bc9e9c7190cfc4e2
RenanSaravalli <renansaravalli@gmail.com>
2025-03-11 11:30:10 -0300

    Ex004: Dissecando uma variável

---
+++ src/exercicios/Ex004.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Ex004 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Digite algo: ");
+        var word = read.nextLine();
+
+        System.out.println("O tipo desse valor: " + word.getClass().getSimpleName());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
