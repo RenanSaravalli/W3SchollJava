55e9314c0f9a50673681492be1dfb1802a1b64bf
RenanSaravalli <renansaravalli@gmail.com>
2025-03-12 09:02:44 -0300

    Como utilizar o While em java

---
+++ src/introduction/loops/WhileLoop.java
@@ -0,0 +1,16 @@
+package introduction.loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        /* while (condition) {
+        *   Code block to be executeda
+        *  }
+        * */
+        int i = 0;
+        while (i < 4) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
