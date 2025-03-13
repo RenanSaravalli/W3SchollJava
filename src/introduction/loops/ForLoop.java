65ff7621bfdc5aa6504e7a8f6195c2ca1ddbd260
RenanSaravalli <renansaravalli@gmail.com>
2025-03-13 09:19:44 -0300

    Como utilizar o For loop em java

---
+++ src/introduction/loops/ForLoop.java
@@ -0,0 +1,17 @@
+package introduction.loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // Quando sabemos exatamente quantas vezes desejamos executar um loop usamos for
+
+        /*
+        * for (statement 1; statement 2; statement 3) {
+        *   // Code Block to be executed
+        * }
+        * */
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
