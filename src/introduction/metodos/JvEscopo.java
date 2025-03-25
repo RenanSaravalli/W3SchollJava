38409b3065f51196372569af4b71956a2b552637
RenanSaravalli <renansaravalli@gmail.com>
2025-03-25 07:53:24 -0300

    Aprendendo sobre o espoco do java

---
+++ src/introduction/metodos/JvEscopo.java
@@ -0,0 +1,20 @@
+package introduction.metodos;
+
+public class JvEscopo {
+    public static void main(String[] args) {
+        // Em Java, variáveis são acessíveis somente dentro da região em que são criadas.
+        // Isso é chamado de escopo
+
+        // Âmbito do métod
+        // Variáveis declaradas diretamente dentro de um métod estão disponíveis em
+        // qualquer lugar do métd após a linha de código na qual foram declaradas.
+
+        // Escopo do bloco
+        // um bloco de código se refere a td código entre chaves {}, que segue a linha
+        // na qual a variável foi declarada
+
+        // Um bloco de código pode existir por si só ou pode pertencer a uma instrução
+        // if, while ou for. No caso de for instruções, as variáveis declaradas na própria
+        // instrução tb estão disponíveis dentro do escopo do bloco.
+    }
+}
