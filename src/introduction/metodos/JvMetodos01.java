33e3e897b23a197160197cca9247b6c6878e3931
RenanSaravalli <renansaravalli@gmail.com>
2025-03-24 23:18:04 -0300

    Inicio aprendendo métodos no java

---
+++ src/introduction/metodos/JvMetodos01.java
@@ -0,0 +1,17 @@
+package introduction.metodos;
+
+public class JvMetodos01 {
+    public static void main(String[] args) {
+        /* Um métod deve ser declarado dentro de uma classe. */
+
+        // Chamando um métod
+        myMethod();
+    }
+
+    static void myMethod() {
+        // Code to be executed
+        System.out.println("O método foi executado!!");
+    }
+    // static significa que o métod pertence à classe Main e não a um obj da classe JvMetodos01
+
+}
