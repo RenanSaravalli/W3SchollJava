0c3188b7f76e864ec145ddb281b76f0513f427e6
RenanSaravalli <renansaravalli@gmail.com>
2025-04-05 21:21:01 -0300

    Exercício #028

---
+++ src/exercicios/Ex028.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+import java.util.Timer;
+
+public class Ex028 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        Timer tempo = new Timer();
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("Vou pensar em um número entre 0 e 5. Tente adivinhar...");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+        System.out.print("Em que número eu pensei? ");
+        int chute = read.nextInt();
+
+    }
+}
