b29e6831c18f8a8a96342c0b7658d296c83d4f21
RenanSaravalli <renansaravalli@gmail.com>
2025-05-13 22:58:13 -0300

    Exercício 28 criando um jogo de números aleatórios

--- src/exercicios/Ex028.java
+++ src/exercicios/Ex028.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 import java.util.Timer;
 
 public class Ex028 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner read = new Scanner(System.in);
         Timer tempo = new Timer();
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         System.out.println("Vou pensar em um número entre 0 e 5. Tente adivinhar...");
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
+        int numeroRandom = (int)(Math.random() * 6);
+
         System.out.print("Em que número eu pensei? ");
         int chute = read.nextInt();
 
+        System.out.println("Processando!!");
+        Thread.sleep(1000);
+
+        if (chute == numeroRandom) {
+            System.out.println("PERDI!! Você acertou o número!!");
+        } else {
+            System.out.println("HAHAHA Ganhei Você não acertou o número!!");
+        }
+
+        System.exit(0);
     }
 }
